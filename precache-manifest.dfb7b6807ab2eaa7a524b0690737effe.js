ae976d28f0cfbfa21f930ffd5d73bea6a8364cf8
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-02-22 17:02:46 +0200

    Updates

---
+++ precache-manifest.dfb7b6807ab2eaa7a524b0690737effe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea653c1b6bac9e66695c0c4be595eb4",
+    "url": "/Movies/index.html"
+  },
+  {
+    "revision": "7deac6e8bcae3e3edd24",
+    "url": "/Movies/static/css/2.09700f6a.chunk.css"
+  },
+  {
+    "revision": "f738135b82a5555c0714",
+    "url": "/Movies/static/css/main.926526b3.chunk.css"
+  },
+  {
+    "revision": "7deac6e8bcae3e3edd24",
+    "url": "/Movies/static/js/2.d6346eba.chunk.js"
+  },
+  {
+    "revision": "f738135b82a5555c0714",
+    "url": "/Movies/static/js/main.0f9e2c72.chunk.js"
+  },
+  {
+    "revision": "ff7444c216c0f17534ff",
+    "url": "/Movies/static/js/runtime~main.a0dd656c.js"
+  },
+  {
+    "revision": "2af82424c8f343486b5f52034baff0f4",
+    "url": "/Movies/static/media/adventures.2af82424.png"
+  },
+  {
+    "revision": "245e6cd1be41fe7d3605df819631436b",
+    "url": "/Movies/static/media/default.245e6cd1.png"
+  },
+  {
+    "revision": "8b2ce634d3d296be0af39b3faa6d3f47",
+    "url": "/Movies/static/media/documentary.8b2ce634.png"
+  },
+  {
+    "revision": "b9aba793000ceb549410b487769e642e",
+    "url": "/Movies/static/media/empty-poster.b9aba793.png"
+  },
+  {
+    "revision": "75723dc5222bcd11ef7c53e4a8aebb5c",
+    "url": "/Movies/static/media/family.75723dc5.png"
+  },
+  {
+    "revision": "bb3991f783b5826cbdc2b364ff8f52be",
+    "url": "/Movies/static/media/horror.bb3991f7.png"
+  },
+  {
+    "revision": "767a2178ef98bf0f85e6931595b56c40",
+    "url": "/Movies/static/media/show.767a2178.png"
+  },
+  {
+    "revision": "73d796179189b2f87f896a1153d3fa3e",
+    "url": "/Movies/static/media/youtube.73d79617.svg"
+  }
+]);
